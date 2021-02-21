8bea5ce065538f50369c9b55a520ebf1ebf9a2eb
Nyerhovwo Adjekughene <nerojust4@gmail.com>
2021-02-21 17:38:23 +0100

    updated versions for libraries used.

--- app/src/main/java/com/intelia/loansdk/MainActivity.kt
+++ app/src/main/java/com/intelia/loansdk/MainActivity.kt
@@ -105,7 +105,7 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         smsQuery()
         return super.onOptionsItemSelected(item)
     }
